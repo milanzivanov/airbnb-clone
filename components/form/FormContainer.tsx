6cbabb494b0e9cc80b77c670067369bb5771a20b
milan <milanzivanov@users.noreply.github.com>
2025-03-14 14:22:05 +0100

    For now I gonna use default theme colors

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useFormState } from "react-dom";
-import { useEffect } from "react";
+import { useEffect, useActionState as useFormState } from "react";
 import { toast } from "sonner";
 import { actionFunction } from "@/utils/types";
 
