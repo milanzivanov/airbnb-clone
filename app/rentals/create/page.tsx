e1a2b440bf2d9a123cfc91e6bce6ae3f9d3c5438
milan <milanzivanov@users.noreply.github.com>
2025-03-24 20:00:59 +0100

    Amenities Input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -8,6 +8,7 @@ import TextAreaInput from "@/components/form/TextAreaInput";
 import CountriesInput from "@/components/form/CountriesInput";
 import ImageInput from "@/components/form/ImageInput";
 import CounterInput from "@/components/form/CounterInput";
+import AmenitiesInput from "@/components/form/AmenitiesInput";
 
 function CreatePropertyPage() {
   return (
@@ -54,6 +55,9 @@ function CreatePropertyPage() {
           <CounterInput detail="bedrooms" />
           <CounterInput detail="beds" />
           <CounterInput detail="baths" />
+
+          <h3 className="text-lg mt-10 mb-6 font-medium">Amenities</h3>
+          <AmenitiesInput />
 
           <SubmitButton text="create rental" className="mt-12" />
         </FormContainer>
