109b108561fcc06cf905ef19c6abcf8dd1af4832
milan <milanzivanov@users.noreply.github.com>
2025-03-24 15:08:15 +0100

    Categories Input and Description Input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -3,39 +3,45 @@ import FormContainer from "@/components/form/FormContainer";
 import { createPropertyAction } from "@/utils/actions";
 import { SubmitButton } from "@/components/form/Buttons";
 import PriceInput from "@/components/form/PriceInput";
+import CategoriesInput from "@/components/form/CategoriesInpu";
+import TextAreaInput from "@/components/form/TextAreaInput";
 
 function CreatePropertyPage() {
   return (
     <section>
-      <section>
-        <h1 className="text-2xl font-semibold mb-8 capitalize">
-          create property
-        </h1>
-        <div className="border p-8 rounded-md">
-          <h3 className="text-lg mb-4 font-medium">General Info</h3>
-          <FormContainer action={createPropertyAction}>
-            <div className="grid md:grid-cols-2 gap-8 mb-4">
-              <FormInput
-                name="name"
-                type="text"
-                label="Name (20 limit)"
-                defaultValue="Cabin in Serbia"
-              />
-              <FormInput
-                name="tagline"
-                type="text "
-                label="Tagline (30 limit)"
-                defaultValue="Dream Getaway Awaits You Here!"
-              />
-              {/* price */}
-              <PriceInput />
-              {/* categories */}
-            </div>
-            {/* text area / description */}
-            <SubmitButton text="create rental" className="mt-12" />
-          </FormContainer>
-        </div>
-      </section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">
+        create property
+      </h1>
+      <div className="border p-8 rounded-md">
+        <h3 className="text-lg mb-4 font-medium">General Info</h3>
+        <FormContainer action={createPropertyAction}>
+          <div className="grid md:grid-cols-2 gap-8 mb-4">
+            <FormInput
+              name="name"
+              type="text"
+              label="Name (20 limit)"
+              defaultValue="Cabin in Serbia"
+            />
+            <FormInput
+              name="tagline"
+              type="text "
+              label="Tagline (30 limit)"
+              defaultValue="Dream Getaway Awaits You Here!"
+            />
+            {/* price */}
+            <PriceInput />
+            {/* categories */}
+            <CategoriesInput />
+          </div>
+          {/* text area / description */}
+          <TextAreaInput
+            name="description"
+            labelText="Description (10 - 1000 Words)"
+          />
+
+          <SubmitButton text="create rental" className="mt-12" />
+        </FormContainer>
+      </div>
     </section>
   );
 }
