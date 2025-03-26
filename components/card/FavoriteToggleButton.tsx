d5a2ba2a0b89c1c2a35cb0e186997b16b9105274
milan <milanzivanov@users.noreply.github.com>
2025-03-26 16:04:25 +0100

    Favorites SigninIn Button

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,15 @@
 import { FaHeart } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
 
-function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return <CardSignInButton />;
+  }
+
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
