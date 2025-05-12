fb89b7d5927999deb5f326b3d0bba7c8b0af6a8a
milan <milanzivanov@users.noreply.github.com>
2025-05-12 20:05:26 +0200

    Booking container/calendar Initial setup

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,3 +1,32 @@
+"use client";
+import { Calendar } from "@/components/ui/calendar";
+import { use, useEffect, useState } from "react";
+// import { useToast } from "@/components/ui/sonner";
+import { DateRange } from "react-day-picker";
+import { useProperty } from "@/utils/store";
+
+import {
+  generateDisabledDates,
+  generateDateRange,
+  defaultSelected,
+  generateBlockedPeriods
+} from "@/utils/calendar";
+
 export default function BookingCalendar() {
-  return <div>BookingCalendar</div>;
+  const currentDate = new Date();
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  useEffect(() => {
+    useProperty.setState({ range: range });
+  }, [range]);
+
+  return (
+    <Calendar
+      mode="range"
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className="mb-4"
+    />
+  );
 }
--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useProperty } from "@/utils/store";
+import ConfirmBooking from "./ConfirmBooking";
+import BookingForm from "./BookingForm";
+
 export default function BookingContainer() {
-  return <div>BookingContainer</div>;
+  const state = useProperty();
+  const { propertyId, price, bookings, range } = state;
+  console.log("BookingContainer", range, propertyId, price, bookings);
+
+  return (
+    <div className="w-full">
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 }
